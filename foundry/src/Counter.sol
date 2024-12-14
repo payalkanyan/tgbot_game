4e76de5e8c31ab0eacd60afec65def41fcb40e97
payalkanyan <youcancallmepayal@gmail.com>
2024-12-14 22:40:48 +0530

    added foundry

--- foundry/src/Counter.sol
+++ foundry/src/Counter.sol
@@ -1,14 +1,46 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract RewardNFT is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter; // Tracks token IDs
+    mapping(address => bool) public hasClaimed; // Tracks claimed NFTs
+
+    event NFTClaimed(address indexed claimer, uint256 tokenId, string tokenURI);
+
+    constructor() ERC721("RewardNFT", "RNFT") {
+        tokenCounter = 0; // Initialize token counter
     }
 
-    function increment() public {
-        number++;
+    /**
+     * @dev Claim an NFT.
+     * @param tokenURI The metadata URI for the NFT.
+     */
+    function claimNFT(string memory tokenURI) external {
+        require(!hasClaimed[msg.sender], "You have already claimed an NFT.");
+
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        hasClaimed[msg.sender] = true;
+        tokenCounter++;
+
+        emit NFTClaimed(msg.sender, newTokenId, tokenURI);
+    }
+
+    /**
+     * @dev Allows the owner to mint NFTs directly.
+     * @param recipient The recipient address.
+     * @param tokenURI The metadata URI for the NFT.
+     */
+    function mintNFT(address recipient, string memory tokenURI) external onlyOwner {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(recipient, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        tokenCounter++;
     }
 }
